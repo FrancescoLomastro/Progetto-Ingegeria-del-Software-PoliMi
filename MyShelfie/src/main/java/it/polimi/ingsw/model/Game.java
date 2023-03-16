1cd765e00ab20fdf92b4b07f404a07d55dc6cbd4
figio <riccardo.figini@mail.polimi.it>
2023-03-16 15:04:12 +0100

    Prova per vedere perché mi crea merga in automtico

--- MyShelfie/src/main/java/it/polimi/ingsw/model/Game.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,6 +1,30 @@
 package it.polimi.ingsw.model;
 
+import javax.swing.*;
+
 public class Game {
-    int prova;
-    float prova1;
+    private final int numPlayers;
+    public Game(int numPlayers){
+        this.numPlayers=numPlayers;
+        //mancano i costruttori delle altre classi
+    }
+    public void setUp(){
+
+    }
+    public void turn(Player player, Couple[] move, int column){
+
+    }
+
+    private boolean checkCommonGoal(Player player){
+
+        return true/*Da modificare*/;
+    }
+
+    private boolean checkMove(Player player, Couple[] move){
+        return true/*Da modificare*/;
+    }
+
+    public Player winner(){
+        return null;/*Da modificare*/
+    }
 }
