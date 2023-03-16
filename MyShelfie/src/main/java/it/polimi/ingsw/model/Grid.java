8a8b9af0a68feabf42159cb27d73cef171f924af
AndreaFerrini3 <andrea2.ferrini@mail.polimi.it>
2023-03-16 15:19:41 +0100

    aggiunta della classe Grid, semplicemente dichiarando gli attributi e scrivendo le signatures dei metodi

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Grid.java
@@ -0,0 +1,29 @@
+package main.java.it.polimi.ingsw.model;
+
+public class Grid {
+
+    private final int dimensionX;
+    private final int dimensionY;
+    private int numPlayers;
+    private int[] objectCardVector; //3*6
+
+    public Grid(int numPlayers){
+
+    }
+
+    public void refill(){
+
+    }
+
+    public boolean needRefill(){
+
+    }
+
+    public boolean moveAvailable(Couple[] move){
+
+    }
+
+    public ObjectCard[] draw(Couple[] move){
+
+    }
+}
