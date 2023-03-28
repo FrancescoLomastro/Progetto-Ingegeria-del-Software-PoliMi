4a3d8b91f70a7f1b56de55eb047cebe481677154
AlbertoAtCode <alberto.aniballi@mail.polimi.it>
2023-03-28 15:15:52 +0200

    Implementazione iniziale algoritmi di soddisfazione degli obiettivi delle carte obiettivo comune. In futuro, da controllare e risolvere possibili errori.

--- MyShelfie/src/main/java/it/polimi/ingsw/model/CommonGoalCard2.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/CommonGoalCard2.java
@@ -3,6 +3,13 @@ package it.polimi.ingsw.model;
 public class CommonGoalCard2 extends CommonGoalCard {
     @Override
     public boolean isSatisfied(Library lib) {
-        return false;
+        String color = library[0][0].getColor();
+        if (color.equals(library[0][4].getColor())
+                && color.equals(library[5][4].getColor())
+                && color.equals(library[5][0].getColor())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- MyShelfie/src/main/java/it/polimi/ingsw/model/CommonGoalCard8.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/CommonGoalCard8.java
@@ -1,7 +1,23 @@
 package it.polimi.ingsw.model;
+
+import java.util.HashSet;
+
 public class CommonGoalCard8 extends CommonGoalCard {
     @Override
     public boolean isSatisfied(Library lib) {
-        return false;
+        int count_col = 0;
+        for(int col=0;col<5;col++) {
+            HashSet<String> s = new HashSet<>();
+            for(int row=0;row<6;row++) {
+                s.add(library[row][col].getColor());
+            }
+            count_col = (s.size()==3) ? count_col+1 : count_col;
+        }
+
+        if (count_col>=3) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
