0edc5506ddb2a80bc07a469d13200762c2592959
AlbertoAtCode <alberto.aniballi@mail.polimi.it>
2023-04-02 11:14:11 +0200

    Implementazione test CommonGoalCard7Test e verifica dei test, correzione ricerca di isSatisfied() in CommonGoalCard7

--- MyShelfie/src/main/java/it/polimi/ingsw/model/Cards/ConcreteCommonCards/CommonGoalCard7.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/model/Cards/ConcreteCommonCards/CommonGoalCard7.java
@@ -25,12 +25,14 @@ public class CommonGoalCard7 extends CommonGoalCard {
         for(int row=0;row<6;row++) {
             HashSet<Color> s = new HashSet<>();
             for(int col=0;col<5;col++) {
-                s.add(library.getLibrary()[row][col].getColor());
+                if (library.getLibrary()[row][col] != null) {
+                    s.add(library.getLibrary()[row][col].getColor());
+                }
             }
             count_row = (s.size()==5) ? count_row+1 : count_row;
         }
 
-        if (count_row==2) {
+        if (count_row>=2) {
             return true;
         } else {
             return false;
