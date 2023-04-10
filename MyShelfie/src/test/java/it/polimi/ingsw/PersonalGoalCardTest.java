70539c4da6428ad3a7581f9a117d54b65e2ba548
AlbertoAtCode <alberto.aniballi@mail.polimi.it>
2023-04-10 20:27:41 +0200

    Soluzione problemi PersonalGoalCardTest e verifica correttezza nuovi test post cambiamenti in library di Lomi.

--- MyShelfie/src/test/java/it/polimi/ingsw/PersonalGoalCardTest.java
+++ MyShelfie/src/test/java/it/polimi/ingsw/PersonalGoalCardTest.java
@@ -13,8 +13,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 public class PersonalGoalCardTest {
     PersonalGoalCard personalGoalCard;
@@ -27,40 +26,35 @@ public class PersonalGoalCardTest {
     public PersonalGoalCardTest(){
         cardGenerator = new CardGenerator();
         personalGoalCard = cardGenerator.generatePersonalGoalCard();
-        library = new Library(5,6);
-    }
-    /**Method used to insert ObjectCard in matrix
-     * @author: Riccardo Figini
-     * @param row row
-     * @param col column
-     * @param color color*/
-    private void insertElement(int row, int col, Color color){
-        ObjectCard objectCards = new ObjectCard("", color , type);
-        library.insertCardInObjectCards(objectCards, row,col);
-    }
-    /**It fills empty cell
-     * @author: Riccardo Figini
-     * */
-    private void fillEmptyPart(){
-        Random random = new Random();
-        for(int i=0; i<library.getNumberOfRows(); i++){
-            for (int j=0; j<library.getNumberOfColumns(); j++){
-                if(library.getLibrary()[i][j]==null)
-                    insertElement(i, j, Color.getEnumFromRelativeInt(random.nextInt(5)));
-            }
-        }
+        library = new Library();
     }
 
     @Before
     public void setUp(){
-        for(int i=0; i<library.getNumberOfRows(); i++){
-            for(int j=0; j<library.getNumberOfColumns(); j++){
-                library.insertCardInObjectCards(null, i,j);
-            }
-        }
+        library = new Library();
     }
     @Test
-    public void countPersonalGoalCardPoints_correctInputAllMatch_correctOutput(){
+    public void countPersonalGoalCardPoints_correctInputAllMatch_correctOutput() {
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<personalGoalCard.getGoalVector().size(); objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(12,personalGoalCard.countPersonalGoalCardPoints(library));
+
+        /*
         Position position;
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
             position = (Position) personalGoalCard.getGoalVector().get(i).getFirst();
@@ -73,10 +67,31 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "Tutte giuste");
         Assert.assertEquals(12,personalGoalCard.countPersonalGoalCardPoints(library));
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput5match_correctOutput(){
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<5; objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(9,personalGoalCard.countPersonalGoalCardPoints(library));
+        /*
         Position position;
+
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
             if(i<5) {
                 position = (Position) personalGoalCard.getGoalVector().get(i).getFirst();
@@ -98,9 +113,29 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "5");
         Assert.assertEquals(9,personalGoalCard.countPersonalGoalCardPoints(library));
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput4match_correctOutput(){
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<4; objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(6,personalGoalCard.countPersonalGoalCardPoints(library));
+        /*
         Position position;
         Color color;
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
@@ -125,9 +160,29 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "4");
         Assert.assertEquals(6,personalGoalCard.countPersonalGoalCardPoints(library));
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput3match_correctOutput(){
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<3; objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(4,personalGoalCard.countPersonalGoalCardPoints(library));
+        /*
         Position position;
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
             if(i<3) {
@@ -150,9 +205,29 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "3");
         Assert.assertEquals(4, personalGoalCard.countPersonalGoalCardPoints(library));
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput2match_correctOutput(){
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<2; objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(2,personalGoalCard.countPersonalGoalCardPoints(library));
+        /*
         Position position;
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
             if(i<2) {
@@ -175,9 +250,29 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "2");
         Assert.assertEquals(personalGoalCard.countPersonalGoalCardPoints(library), 2);
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput1match_correctOutput(){
+        HashMap<Position,Color> objectCardsGoal = new HashMap<>();
+        for(int objectCard_i=0;objectCard_i<1; objectCard_i++) {
+            objectCardsGoal.put(
+                    (Position) personalGoalCard.getGoalVector().get(objectCard_i).getFirst(),
+                    (Color) personalGoalCard.getGoalVector().get(objectCard_i).getSecond());
+        }
+
+        for(int row=5;row>=0;row--) {
+            for(int col=0;col<library.getNumberOfColumns();col++) {
+                Position position = new Position(row,col);
+                if(objectCardsGoal.get(position)==null) {
+                    library.insertCardsInLibrary(col, new ObjectCard("", Color.BLUE, Type.FIRST));
+                } else {
+                    library.insertCardsInLibrary(col, new ObjectCard("", (Color) objectCardsGoal.get(position), Type.FIRST ));
+                }
+            }
+        }
+        Assert.assertEquals(1,personalGoalCard.countPersonalGoalCardPoints(library));
+        /*
         Position position;
         for(int i=0; i<personalGoalCard.getGoalVector().size(); i++) {
             if(i<1) {
@@ -200,12 +295,14 @@ public class PersonalGoalCardTest {
         fillEmptyPart();
         //printLibrary(library.getLibrary(), personalGoalCard.getGoalVector(), "1");
         Assert.assertEquals(personalGoalCard.countPersonalGoalCardPoints(library), 1);
+         */
     }
     @Test
     public void countPersonalGoalCardPoints_correctInput0match_correctOutput(){
         Assert.assertEquals(personalGoalCard.countPersonalGoalCardPoints(library), 0);
     }
 
+    /*
     @After
     public void tearDown() {
         for(int i=0; i<library.getNumberOfRows(); i++){
@@ -214,24 +311,5 @@ public class PersonalGoalCardTest {
             }
         }
     }
-
-    /*public void printLibrary(ObjectCard[][] objectCards, ArrayList<Couple> couple, String from){
-        System.out.println(from + ", Obbiettivi: ");
-        Position position;
-        Color color;
-        for(int i=0; i< 6; i++){
-            position = (Position) couple.get(i).getFirst();
-            color = (Color) couple.get(i).getSecond();
-            System.out.println("Row: "+ position.getRow() + ", Column: "+ position.getColumn() + ", Color: "+ color);
-        }
-        System.out.println("");
-        System.out.println("");
-        for(int i=0; i<6; i++){
-            for(int j=0; j<5; j++){
-                color = objectCards[i][j].getColor();
-                System.out.print("|row:"+i+",col:"+j+"  " + color +"   ");
-            }
-            System.out.println("");
-        }
-    }*/
+    */
 }
