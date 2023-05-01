ae714ca200397ea79c7b6d11e79bae236977ea6d
AlbertoAtCode <alberto.aniballi@mail.polimi.it>
2023-05-01 10:06:49 +0200

    Risoluzione problemi module-info.java e pox.xml con conseguente verifica del funzionamento del primo stage.

--- MyShelfie/src/main/java/module-info.java
+++ MyShelfie/src/main/java/module-info.java
@@ -1,11 +1,29 @@
 module MyShelfie {
-    requires javafx.controls;
     requires javafx.fxml;
-    requires java.rmi;
+    requires javafx.controls;
     requires com.google.gson;
+    requires java.rmi;
 
-    opens it.polimi.ingsw.View;
+    opens it.polimi.ingsw to javafx.fxml;
+    exports it.polimi.ingsw;
+    exports it.polimi.ingsw.controller;
     exports it.polimi.ingsw.controller.guiControllers;
-    exports it.polimi.ingsw.Network.Servers.RMI;
+    exports it.polimi.ingsw.exceptions;
+    exports it.polimi.ingsw.model;
+    exports it.polimi.ingsw.model.CardGenerator;
+    exports it.polimi.ingsw.model.Cards;
+    exports it.polimi.ingsw.model.Cards.ConcreteCommonCards;
+    exports it.polimi.ingsw.model.Enums;
+    exports it.polimi.ingsw.model.LivingRoom;
+    exports it.polimi.ingsw.model.Player;
+    exports it.polimi.ingsw.model.Utility;
     exports it.polimi.ingsw.Network.Client;
+    exports it.polimi.ingsw.Network.Client.RMI;
+    exports it.polimi.ingsw.Network.Client.Socket;
+    exports it.polimi.ingsw.Network.Messages;
+    exports it.polimi.ingsw.Network.ObserverImplementation;
+    exports it.polimi.ingsw.Network.Servers;
+    exports it.polimi.ingsw.Network.Servers.RMI;
+    exports it.polimi.ingsw.Network.Servers.Socket;
+    exports it.polimi.ingsw.View;
 }
