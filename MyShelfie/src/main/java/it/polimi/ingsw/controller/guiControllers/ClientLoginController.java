33966618e4ff4a741956ee6d3f647f17146504f8
AndreaFerrini3 <andrea2.ferrini@mail.polimi.it>
2023-05-02 20:30:42 +0200

    (per provare) Creazione tasto "start game" sulla schermata iniziale. Dovrebbe invocare il main su ClientApp

--- MyShelfie/src/main/java/it/polimi/ingsw/controller/guiControllers/ClientLoginController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/controller/guiControllers/ClientLoginController.java
@@ -1,4 +1,14 @@
 package it.polimi.ingsw.controller.guiControllers;
 
+import it.polimi.ingsw.ClientApp;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class ClientLoginController {
+    @FXML
+    public void startGame() {
+
+        String[] args = {};
+        ClientApp.main(args);
+    }
 }
