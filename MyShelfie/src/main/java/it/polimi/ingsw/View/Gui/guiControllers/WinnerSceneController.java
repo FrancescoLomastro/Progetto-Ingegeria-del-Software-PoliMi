4175acc59dca4919aff16314367e55ddf61db2bd
AndreaFerrini3 <andrea2.ferrini@mail.polimi.it>
2023-05-16 17:19:30 +0200

    WinnerScene + immagine (provvisoria) per la schermata finale

--- MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/guiControllers/WinnerSceneController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/guiControllers/WinnerSceneController.java
@@ -1,10 +1,12 @@
 package it.polimi.ingsw.View.Gui.guiControllers;
 
+import it.polimi.ingsw.model.Cards.ObjectCard;
 import it.polimi.ingsw.model.Player.Player;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,8 +14,13 @@ import java.util.ResourceBundle;
 public class WinnerSceneController implements Initializable {
 
     private Player winner;
-    private String username;
-    private
+    private Player[] others;
+    public Label title_label;
+    public Label second;
+    public Label third;
+    public Label fourth;
+    private ObjectCard[][] winnerLibrary;
+
     @FXML
     Button button;
     @FXML
@@ -24,6 +31,13 @@ public class WinnerSceneController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        winnerLibrary = winner.getLibrary().getMatrix();
 
+        title_label.setText("player " + winner.getName() + " won!");
+
+        //fare i controlli sul numero di giocatori
+        second.setText("2nd: " + others[0].getName());
+        third.setText("3rd: " + others[1].getName());
+        fourth.setText("4th: " + others[2].getName());
     }
 }
