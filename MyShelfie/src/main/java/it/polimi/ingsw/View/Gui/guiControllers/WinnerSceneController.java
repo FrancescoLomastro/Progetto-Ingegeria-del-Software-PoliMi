3fe4dfd8b2375c5ac38a785f3861357f031c6f57
AndreaFerrini3 <andrea2.ferrini@mail.polimi.it>
2023-05-16 16:40:18 +0200

    inzio di WinnerScene

---
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/guiControllers/WinnerSceneController.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.View.Gui.guiControllers;
+
+import it.polimi.ingsw.model.Player.Player;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WinnerSceneController implements Initializable {
+
+    private Player winner;
+    private String username;
+    private
+    @FXML
+    Button button;
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        // aprire la libreria del vincitore
+
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+}
