45d7212652a5674d7aab4951ad8b914dd5a98ed9
AndreaFerrini3 <andrea2.ferrini@mail.polimi.it>
2023-05-26 14:08:39 +0200

    WinnerScene, non ho ancora modificato l'estetica, ma il resto funziona

--- MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/guiControllers/WinnerSceneController.java
+++ MyShelfie/src/main/java/it/polimi/ingsw/View/Gui/guiControllers/WinnerSceneController.java
@@ -1,46 +1,81 @@
 package it.polimi.ingsw.View.Gui.guiControllers;
 
 import it.polimi.ingsw.model.Cards.ObjectCard;
-import it.polimi.ingsw.model.Player.Player;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+
+// MANCA L'INVOCAZIONE DELLA SCHERMATA AL MOMENTO GIUSTO
 
 public class WinnerSceneController implements Initializable {
 
-    private Player winner;
-    private Player[] others;
-    public Label title_label;
-    public Label second;
-    public Label third;
-    public Label fourth;
-    private ObjectCard[][] winnerLibrary;
+    private  int i;
+    private String winner;
+    private String[] others;
+    @FXML
+    public Label winner_label;
+    @FXML
+    public Label second_label;
+    @FXML
+    public Label third_label;
+    @FXML
+    public Label fourth_label;
 
     @FXML
-    Button button;
+    Button button_show_library;
+    ObjectCard[][] winnerLibrary;
+    Map<String, Integer> points;
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        // aprire la libreria del vincitore
+    private void handleButtonAction(String winner) {
 
+        ViewFactory.getInstance().onLibraryClick(winner);
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        winnerLibrary = winner.getLibrary().getMatrix();
+        points = new HashMap<>();
 
-        title_label.setText("player " + winner.getName() + " won!");
+        winner_label.setText("player " + winner + " won!");
 
-        //fare i controlli sul numero di giocatori
-        second.setText("2nd: " + others[0].getName());
-        third.setText("3rd: " + others[1].getName());
-        fourth.setText("4th: " + others[2].getName());
+        button_show_library.setOnMouseClicked(mouseEvent -> {
+
+            handleButtonAction(winner);
+        });
+
+        // non va bene perchè non è ordinato in base ai punteggi
+        others = ViewFactory.getInstance().getClientModel().getPlayerNames();
+        if(others.length > 1) {
+            second_label.setText("2nd: " + others[1]);
+        }
+
+        if(others.length > 2) {
+            third_label.setText("3rd: " + others[2]);
+        }
+
+        if(others.length > 3) {
+            fourth_label.setText("4th: " + others[3]);
+        }
+    }
+
+    public String getWinner() {
+        return winner;
     }
 
     public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public ObjectCard[][] getWinnerLibrary() {
+        return winnerLibrary;
+    }
+
+    public void setWinnerLibrary(ObjectCard[][] winnerLibrary) {
+        this.winnerLibrary = winnerLibrary;
     }
 }
