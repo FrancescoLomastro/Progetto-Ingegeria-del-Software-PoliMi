0dde53e160f0c598f6f4a2e6406d829e4aae86f4
Francesco Gregorio Lo Mastro <francescogregorio.lomastro@mail.polimi.it>
2023-06-26 14:05:13 +0200

    javadoc exceptions package

--- src/main/java/it/polimi/ingsw/exceptions/InvalidMoveException.java
+++ src/main/java/it/polimi/ingsw/exceptions/InvalidMoveException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.exceptions;
 
+/**
+ * Thrown when a move is estimated as not valid in the model, reasons can be many
+ */
 public class InvalidMoveException extends Exception{
     public InvalidMoveException(String message){
         super(message);
--- src/main/java/it/polimi/ingsw/exceptions/ResetMoveException.java
+++ src/main/java/it/polimi/ingsw/exceptions/ResetMoveException.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.exceptions;
 
+/**
+ * Thrown when the user wants to reset his move in the CLI
+ */
 public class ResetMoveException extends Exception{
     public ResetMoveException(){
         super();
